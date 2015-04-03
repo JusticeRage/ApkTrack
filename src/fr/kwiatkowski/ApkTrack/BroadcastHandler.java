b4140830e08d4675e8ddd6a2bce4c0c20f3744e9
JusticeRage <justicerage@manalyzer.org>
2015-04-03 22:29:25 +0200

    Consolidated notifications. Fixes #32.

--- src/fr/kwiatkowski/ApkTrack/BroadcastHandler.java
+++ src/fr/kwiatkowski/ApkTrack/BroadcastHandler.java
@@ -19,12 +19,15 @@ package fr.kwiatkowski.ApkTrack;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class BroadcastHandler extends BroadcastReceiver
 {
@@ -63,17 +66,50 @@ public class BroadcastHandler extends BroadcastReceiver
 
         if (res.getStatus() == VersionGetResult.Status.UPDATED)
         {
+            // Get all updated apps
+            ArrayList<InstalledApp> updated_apps = new ArrayList<InstalledApp>();
+            for (InstalledApp ia : AppPersistence.getInstance(context).getStoredApps())
+            {
+                if (ia.isUpdateAvailable()) {
+                    updated_apps.add(ia);
+                }
+            }
+
             Resources r = context.getResources();
-            // Show a notification for updated apps
             Notification.Builder b = new Notification.Builder(context);
-            b.setContentTitle(r.getString(R.string.app_updated_notification, app.getDisplayName()))
-                    .setContentText(r.getString(R.string.app_version_available, app.getLatestVersion()))
-                    .setTicker(r.getString(R.string.app_can_be_updated, app.getDisplayName()))
-                    .setSmallIcon(R.drawable.ic_menu_refresh)
-                    .setAutoCancel(true); //TODO: Think about launching the search on user click.
+            // Show a notification for updated apps
+            if (updated_apps.size() == 0)
+            {
+                b.setContentTitle(r.getString(R.string.app_updated_notification, app.getDisplayName()))
+                        .setContentText(r.getString(R.string.app_version_available, app.getLatestVersion()))
+                        .setTicker(r.getString(R.string.app_can_be_updated, app.getDisplayName()))
+                        .setSmallIcon(R.drawable.ic_menu_refresh)
+                        .setAutoCancel(true); //TODO: Think about launching the search on user click.
+            }
+            else
+            {
+                b.setContentTitle(r.getString(R.string.apps_updated_notification))
+                        .setContentText(r.getString(R.string.apps_updated_notification_summary, app.getDisplayName(), updated_apps.size()))
+                        .setTicker(r.getString(R.string.apps_updated_notification))
+                        .setSmallIcon(R.drawable.ic_menu_refresh)
+                        .setAutoCancel(true);
+
+                Notification.InboxStyle style = new Notification.InboxStyle();
+                for (InstalledApp ia : updated_apps) {
+                    style.addLine(r.getString(R.string.app_version_available_2, ia.getDisplayName(), ia.getVersion(), ia.getLatestVersion()));
+                }
+                style.setBigContentTitle(r.getString(R.string.apps_updated_notification));
+                b.setStyle(style);
+            }
+
+            // Open ApkTrack when the notification is clicked.
+            Intent i = new Intent();
+            i.setClass(context, MainActivity.class);
+            PendingIntent pi = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_ONE_SHOT);
+            b.setContentIntent(pi);
 
             NotificationManager mgr = (NotificationManager) context.getSystemService(Service.NOTIFICATION_SERVICE);
-            mgr.notify(app.getDisplayName().hashCode(), b.build()); // One notification per available update.
+            mgr.notify(1, b.build()); // Consolidate notifications.
             action_on_activity_focus_gain = reload_action.RELOAD;
         }
         else if (res.getStatus() == VersionGetResult.Status.ERROR && app.isLastCheckFatalError()) {
